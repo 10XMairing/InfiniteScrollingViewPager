6678dd0074747d1b95d721c9795f1e738a56b33a
10XMairing <mairingdao@gmail.com>
2018-08-12 15:09:33 +0530

    added a timer

--- app/src/main/java/com/example/tenx/infinitescrollingviewpager/MainActivity.java
+++ app/src/main/java/com/example/tenx/infinitescrollingviewpager/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     CustomAdapter adapter;
@@ -19,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
         pager = findViewById(R.id.pager);
         pager.addOnPageChangeListener(new CircularViewPagerHandler(pager, adapter));
         pager.setAdapter(adapter);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                pager.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int currentItem = pager.getCurrentItem();
+                        pager.setCurrentItem(currentItem+1, true);
+                    }
+                });
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 500, 2300);
+
 
     }
 }
